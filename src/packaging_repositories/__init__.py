cc1d656546c5969e387443cb8f049a65c24e51a4
Tzu-ping Chung <uranusjr@gmail.com>
2018-09-27 14:29:01 +0800

    Expose match_egg_info_version

--- src/packaging_repositories/__init__.py
+++ src/packaging_repositories/__init__.py
@@ -3,7 +3,7 @@ __all__ = [
     "Entry", "Fetcher",
     "Filter", "RequiresPythonFilter", "VersionFilter",
     "FlatHTMLRepository", "LocalDirectoryRepository", "SimpleRepository",
-    "Endpoint", "guess_encoding",
+    "Endpoint", "guess_encoding", "match_egg_info_version",
 ]
 
 from .entries import Entry
@@ -12,6 +12,6 @@ from .filters import Filter, RequiresPythonFilter, VersionFilter
 from .repositories import (
     FlatHTMLRepository, LocalDirectoryRepository, SimpleRepository,
 )
-from .utils import Endpoint, guess_encoding
+from .utils import Endpoint, guess_encoding, match_egg_info_version
 
 __version__ = "0.3.0d1"
